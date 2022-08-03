faf7c17e6d5d8a732b8a4822332a198cc659d46e
Gudmundur Bjornsson <geb22@hi.is>
2022-08-03 16:39:13 +0000

    tenglar uppfærðir

--- components/SocialMedia.jsx
+++ components/SocialMedia.jsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 const SocialMedia = () => (
   <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
     <h3 className="text-xl mb-4 font-semibold border-b pb-4">Tenglar</h3>
-    <Link href="https://www.facebook.com">
+    <Link href="https://www.facebook.com/thjalfi/">
       <span className="cursor-pointer block border-b pb-3 mb-3 text-blue">
         FACEBOOK
       </span>
     </Link>
-    <Link href="https://www.instagram.com">
+    <Link href="https://instagram.com/hestamannafelagid.thjalfi?igshid=YmMyMTA2M2Y=">
       <span className="cursor-pointer block border-b pb-3 mb-3">
         INSTAGRAM
       </span>
